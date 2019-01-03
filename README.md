c2a0b388e06f2ff01061b8c442ef45238628c163
yangpeicheng <18362903155@163.com>
2019-01-03 13:22:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Gait Analysis Demo HTML5
+========
+
+[animation ref](https://codepen.io/yangpeicheng/pen/xmRXyb)
+
+![demo](img/demo.PNG)
---
+++ js/xvrpee.js
@@ -0,0 +1,3 @@
+var things = "<a id='DiacoDesignLink' href='https://diacodesign.com' class='fa fa-dot-circle-o' target='_blank'><span>DiacoDesign.com</span></a><a id='pens' href='https://codepen.io/collection/DgYoEw' class='fa fa-codepen' target='_blank'><span>my other Pens</span></a>";
+document.body.innerHTML+=things;
+
